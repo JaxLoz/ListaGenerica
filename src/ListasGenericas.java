371414f48b98fe66c2c9602c2b750ebe53a38f88
JavirKali <jaxloz2002@gmail.com>
2021-12-13 21:08:01 -0500

    Prueba 1

--- src/ListasGenericas.java
+++ src/ListasGenericas.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
-/**
- *
- * @author javier
- */
+ // @author javier
+ 
 public class ListasGenericas {
 
     /**
@@ -16,6 +8,7 @@ public class ListasGenericas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
